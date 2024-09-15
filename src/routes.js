ae9382edc671083d7e3bf08c4ce0ad1d995263d4
Mussinady Abubacar <mussinadyabubacar@gmail.com>
2024-09-15 08:41:02 +0000

    :sparkles: feat: adding  method get

--- src/routes.js
+++ src/routes.js
@@ -2,3 +2,15 @@ import { randomUUID } from 'node:crypto';
 import { DataBase } from './controllers/database.js';
 
 const dataBase = new DataBase()
+
+export const routes = [
+    {
+        method: 'GET',
+        path: '/users',
+        handler: (req, res) => {
+            const users = dataBase.select('users')
+
+            return res.end(JSON.stringify(users))
+        }
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 import http from 'node:http';
+import { bodyToJson } from './middlewares/bodytojson';
+import { routes } from './routes.js';
 
 const port = 3000;
 const host = 'localhost'
 
 const server = http.createServer(async (req, res) => {
+    const { method, url } = req
+
+    await bodyToJson(req, res)
+
+    const route = routes.find(route => {
+        return route.method === method && route.path === url
+    })
+
+    if(route) {
+        return route.handler(req, res)
+    }
+    
     return res  
         .writeHead(404)
         .end(JSON.stringify({error: 'Resource not Found...'}))
