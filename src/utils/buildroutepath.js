0a1eab170c32bfe2f22ed3050ffafce51c7b43ce
Mussinady Abubacar <mussinadyabubacar@gmail.com>
2024-09-15 09:01:32 +0000

    :wrench: chore: setting the buildroutepath to capture the route params

---
+++ src/utils/buildroutepath.js
@@ -0,0 +1,7 @@
+export function buildRoutePath(path) {
+    const routeParamsRegexp = /:([a-zA-Z]+)/g
+    const routePath = path.replaceAll(routeParamsRegexp, '(?<$1>(a-z0-9\-_)+)')
+    const routeRegexp = new RegExp(`^${routePath}(?<query>\\?(.*))?$`)
+
+    return routeRegexp
+}
